953a948a5eccb54bff8400dbea2083d3624389c9
jackfretwell <jackfretwell99@icloud.com>
2026-08-31 15:11:51 +0100

    Add metrics an reset handlers

--- main.go
+++ main.go
@@ -4,16 +4,44 @@ import(
 	"net/http"
 	"time"
 	"log"
+	"sync/atomic"
+	"fmt"
 )
 
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) writeNumberOfRequests(w http.ResponseWriter, req *http.Request) {
+	hits := cfg.fileserverHits.Load()
+	formattedText := fmt.Sprintf("Hits: %d", hits)
+	w.Write([]byte(formattedText))
+}
+
+func (cfg *apiConfig) resetFileserverHits (w http.ResponseWriter, req *http.Request) {
+	cfg.fileserverHits.Store(0)
+}
+
+
 func main() {
+	cfg := apiConfig{}
 	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	mux.Handle("/app/", http.StripPrefix("/app", cfg.middlewareMetricsInc(http.FileServer(http.Dir(".")))))
 	mux.HandleFunc("/healthz", func(w http.ResponseWriter, req *http.Request) {
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("OK"))
 	})
+
+	mux.HandleFunc("/metrics", cfg.writeNumberOfRequests)
+	mux.HandleFunc("/reset", cfg.resetFileserverHits)
 
 	s := &http.Server {
 		Addr:			":8080",
